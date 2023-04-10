dcdcc51f893d286517c9dbf5b94320936f91e1ce
Nexlson <nelsonchoon112@gmail.com>
2023-04-10 20:51:55 +0800

    first version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rust-cli
 A CLI build using rust
+
+
+## What?
+- cli for shortcuts that i normally used
+- to store scripts that i usually need for work/hobby
+- installable CLI command line tool
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use clap::Parser;
+use std::process::Command;
+use std::env;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// script to run
+    #[arg(short, long)]
+    file: String,
+
+    #[arg(short, long, default_value = "work")]
+    mode: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let filename_path = format!("/src/scripts/{}.sh", args.file);
+    println!("Running scripts from {}", filename_path);
+
+
+    // get current DIR
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let dir_string = current_dir.clone().into_os_string().into_string().expect("Failed to convert PathBuf to String");
+
+    // run bash file
+    let output = Command::new("bash")
+        .arg(dir_string + &filename_path)
+        .output()
+        .expect("Failed to run Bash scripts");
+    if output.status.success(){
+        println!("Done executing your script!")
+    }else {
+        println!("Failed to execute your script!");
+    }
+}
